cd5f5e49b11dd1ee4a00486b190ff7f3322294cc
samish <samish707@gmail.com>
2022-09-24 12:33:39 +0530

    Commit 2

--- src/P1/P2.java
+++ src/P1/P2.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 
 
 public class P2 {
-
+int i=10;
 	public static void main(String[] args) {
 		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\SBGI\\Desktop\\chromedriver_win32\\chromedriver.exe");  
 		    WebDriver driver=new ChromeDriver();  
