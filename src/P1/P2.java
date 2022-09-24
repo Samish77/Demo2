fe12c97cac8c9793703d389dd7a4b9ec4fbe0e46
samish <samish707@gmail.com>
2022-09-24 12:31:10 +0530

    commit 1

---
+++ src/P1/P2.java
@@ -0,0 +1,21 @@
+package P1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+
+
+public class P2 {
+
+	public static void main(String[] args) {
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\SBGI\\Desktop\\chromedriver_win32\\chromedriver.exe");  
+		    WebDriver driver=new ChromeDriver();  
+		      
+		// Launch website  
+		    driver.navigate().to("http://www.google.com/");  
+		    System.out.println(driver.getTitle());
+
+	}
+
+}
